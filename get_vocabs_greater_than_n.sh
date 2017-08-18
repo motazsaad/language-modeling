9acc64015393270184402fd4f452c16b3f4ded45
Motaz Saad <motaz.saad@gmail.com>
2017-08-18 23:35:54 +0300

    generalize get_vocabs_gt

--- get_vocabs_greater_than_n.sh
+++ get_vocabs_greater_than_n.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
-source ~/py3env/bin/activate
-n=5
+if [ $# -ne 2 ]; then
+    echo "usage ${0} gt text_dir vocab_dir";
+    echo "gt: words that have frequency greater than gt";
+    echo "text_dir: text data directory";
+    echo "vocab_dir: vocab directory";
+    exit 1;
+fi
 
-printf "words greater than  %s\n" "${n}"
+source ~/py3env/bin/activate
+
+g=${1}
+vocab_dir=${2}
+mkdir -p ${vocab_dir}
+
+printf "words greater than  %s\n" "${g}"
 for corpus in train/*.arb
 do
     filename=$(basename "$corpus")
     corpus_name="${filename%%.*}"
     printf "%s\n" "generating vocab for ${corpus}"
-    python wordfreq2vocab.py -t ${corpus} -v vocabs/${corpus_name}.gt${n}.vocab -gt ${n}
+    python wordfreq2vocab.py -t ${corpus} \
+    -v vocabs/${corpus_name}.gt${g}.vocab \
+    -f vocabs/${corpus_name}.gt${g}.freq \
+    -gt ${g}
 done
