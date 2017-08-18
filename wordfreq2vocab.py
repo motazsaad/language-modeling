bf2ef615238a072f171f76c1e1c970e7b29bce4f
Motaz Saad <motaz.saad@gmail.com>
2017-08-18 23:29:37 +0300

    generalize wordfreq2vocab

--- wordfreq2vocab.py
+++ wordfreq2vocab.py
@@ -7,6 +7,8 @@ parser.add_argument('-t', '--text', type=argparse.FileType(mode='r', encoding='u
                     help='text corpus', required=True)
 parser.add_argument('-v', '--vocabulary', type=argparse.FileType(mode='w', encoding='utf-8'),
                     help='the output vocabulary file', required=True)
+parser.add_argument('-f', '--frequency', type=argparse.FileType(mode='w', encoding='utf-8'),
+                    help='frequency file', required=True)
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-top', type=int, help='top N words (most N frequent words)')
@@ -14,18 +16,21 @@ group.add_argument('-gt', type=int, help='words that have frequency greater than
 group.add_argument('-all', action='store_true', help='all words (vocabulary) in the corpus')
 
 
-def get_most_n_frequent(text, outfile, top):
+def get_most_n_frequent(text, outfile, freq_file, top):
     word_counter = Counter()
     word_counter.update(text.split())
+    top_words = word_counter.most_common(top)
     outfile.write('\n'.join([w for w, f in word_counter.most_common(top)]))
     outfile.write('\n')
+    freq_file.write('\n'.join(["{}\t{}".format(w, f) for w, f in top_words]))
 
 
-def get_freqs_gt(text, outfile, gt):
+def get_freqs_gt(text, outfile, freq_file, gt):
     word_counter = Counter()
     word_counter.update(text.split())
     outfile.write('\n'.join([w for w, f in word_counter.items() if f > gt]))
     outfile.write('\n')
+    freq_file.write('\n'.join(["{}\t{}".format(w, f) for w, f in word_counter.items() if f > gt]))
 
 
 def get_all_vocab(text, outfile):
@@ -43,10 +48,11 @@ if __name__ == '__main__':
     print('text |w|={}'.format(len(infile.split())))
     print('text |v|={}'.format(len(set(infile.split()))))
     outfile = args.vocabulary
+    freq_file = args.frequency
     if args.top:
-        get_most_n_frequent(infile, outfile, args.top)
+        get_most_n_frequent(infile, outfile, freq_file, args.top)
     if args.gt:
-        get_freqs_gt(infile, outfile, args.gt)
+        get_freqs_gt(infile, outfile, freq_file, args.gt)
     if args.all:
         get_all_vocab(infile, outfile)
 
