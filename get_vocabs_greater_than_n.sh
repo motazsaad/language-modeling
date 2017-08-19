07826b342b42cd4e0ee2e066b4d451e6f6f0a666
Motaz Saad <motaz.saad@gmail.com>
2017-08-19 10:02:55 +0300

    generalize

--- get_vocabs_greater_than_n.sh
+++ get_vocabs_greater_than_n.sh
@@ -27,7 +27,7 @@ do
     filename=$(basename "$corpus")
     corpus_name="${filename%%.*}"
     printf "generating vocab for %s \t words greater than %s \n" " ${corpus} ${g}"
-    python wordfreq2vocab.py -t ${corpus} \
+    python ${py_dir}/wordfreq2vocab.py -t ${corpus} \
     -v vocabs/${corpus_name}.gt${g}.vocab \
     -f vocabs/${corpus_name}.gt${g}.freq \
     -gt ${g}
